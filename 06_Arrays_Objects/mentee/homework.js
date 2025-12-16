84be414cfaf1a6f8f0a08bc9ba8f20dacac924c8
Jessica Wong <116975948+jess-icaww@users.noreply.github.com>
2025-12-16 00:25:13 -0500

    Add HW6

--- 06_Arrays_Objects/mentee/homework.js
+++ 06_Arrays_Objects/mentee/homework.js
@@ -3,6 +3,18 @@
 
 // STEP 1: Create an array called contacts.
 //         It should contain at least 2 objects.
+contacts = [
+    {
+        name: "Jessica",
+        phone: "123-456-7891",
+        isFavorite: true
+    },
+    {
+        name: "Sophia",
+        phone: "198-765-4321",
+        isFavorite: true
+    }
+];
 
 // STEP 2: Each contact object needs:
 //         name (string)
@@ -10,11 +22,20 @@
 //         isFavorite (boolean)
 
 // STEP 3: Log the total number of contacts.
+console.log(contacts.length);
 
 // STEP 4: Log the name of the first contact.
+console.log(contacts[0].name);
 
 // STEP 5: Update one contact’s isFavorite value.
+contacts[1].isFavorite = false;
 
 // STEP 6: Add (push) a new contact object.
+contacts.push({
+    name: "Hope",
+    phone: "111-111-1111",
+    isFavorite: false
+});
 
 // STEP 7: Log the updated contacts array.
+console.log(contacts);
--- 06_Arrays_Objects/mentee/script.js
+++ 06_Arrays_Objects/mentee/script.js
@@ -6,6 +6,9 @@
 // STEP 1: Create an array called favoriteFruits with at least 3 fruits.
 // STEP 2: Log the entire array.
 // STEP 3: Log the first and second items using indexes.
+let favoriteFruits = ["banana", "apple", "grape"];
+console.log(favoriteFruits);
+console.log(favoriteFruits[0], favoriteFruits[1]);
 
 // -------- TASK 2 — MODIFY AN ARRAY --------
 
@@ -13,16 +16,29 @@
 // STEP 5: Update one existing fruit (change its value).
 // STEP 6: "Remove" a fruit by setting it to null or an empty string.
 // STEP 7: Log the updated array.
-
+favoriteFruits[3] = "pear";
+favoriteFruits[0] = "mango";
+favoriteFruits[3] = "";
+console.log(favoriteFruits);
 // -------- TASK 3 — CREATE A SIMPLE OBJECT --------
 
 // STEP 8: Create an object called student with:
 //         name, age, isEnrolled (boolean)
 // STEP 9: Log the object.
 // STEP 10: Log individual properties (student.name, student.age)
+let student = {
+    name: "Jessica",
+    age: "22",
+    isEnrolled: true
+};
+console.log(student);
+console.log(student.name, student.age, student.isEnrolled);
 
 // -------- TASK 4 — MODIFY OBJECT PROPERTIES --------
 
 // STEP 11: Update the student's age.
 // STEP 12: Add a new property called favoriteColor.
 // STEP 13: Log the updated object.
+student.age = "23";
+student.favoriteColor = "Maroon";
+console.log(student);
